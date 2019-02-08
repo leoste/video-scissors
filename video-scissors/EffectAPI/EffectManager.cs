f9c353adad6427380bafef68da019aeab936c056
GitWither <wither.tweets@gmail.com>
2019-02-08 15:54:46 +0100

    Haha did you think of a good description??? JK! Here is one: started working on plugin api

---
+++ video-scissors/EffectAPI/EffectManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scissors.EffectAPI
+{
+    class EffectManager
+    {
+        public static List<Assembly> effects = new List<Assembly>();
+        static void LoadEffect(string path)
+        {
+            try
+            {
+                Assembly effect = Assembly.LoadFile(path);
+                effects.Add(effect);
+            }
+            catch (Exception e)
+            {
+                Debug.Fail(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ video-scissors/EffectAPI/IEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scissors.EffectAPI
+{
+    interface IEffect
+    {
+        string Name { get; set; }
+        int[] Version { get; set; }
+        
+        void Load();
+        void Update();
+        void Shutdown();
+    }
+}
