78457dbde2796ca815b79f1f3fa81d4cfc052f18
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2019-02-16 02:37:52 +0200

    Reorganised GlobalConfig files and namespace

---
+++ video-scissors/Config/Project_config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scissors.Config
+{
+    public static partial class GlobalConfig
+    {
+        private static int defaultProjectFramerate = 30;
+        private static int defaultProjectFrameWidth = 1920;
+        private static int defaultProjectFrameHeight = 1080;
+
+        public static int DefaultProjectFramerate { get { return defaultProjectFramerate; } }
+        public static int DefaultProjectFrameWidth { get { return defaultProjectFrameWidth; } }
+        public static int DefaultProjectFrameHeight { get { return defaultProjectFrameHeight; } }
+    }
+}
