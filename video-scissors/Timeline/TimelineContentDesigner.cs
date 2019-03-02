15fc5e3e82464c4c2b1663fa7445ce3453234db1
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2019-03-02 17:51:18 +0200

    Made TimelineContent look better in designer

--- video-scissors/Timeline/TimelineContentDesigner.cs
+++ video-scissors/Timeline/TimelineContentDesigner.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,27 @@ namespace Scissors.Timeline
         private class TimelineContentDesigner : ControlDesigner
         {
             protected override void OnPaintAdornments(PaintEventArgs pe)
-            {
+            {                
                 if (!(Control is TimelineContent control)) return;
+                
+                pe.Graphics.FillRectangle(new SolidBrush(control.BackColor), control.ContainerRectangle);
 
-                Brush rulerBrush = new SolidBrush(Color.FromArgb(128, Color.CornflowerBlue));
-                pe.Graphics.FillRectangle(rulerBrush, control.RulerContainerRectangle);
+                Color textColor = SelectHighestContrast(control.BackColor, Color.DimGray, Color.LightGray);
 
-                Brush slicesBrush = new SolidBrush(Color.FromArgb(128, Color.LightGoldenrodYellow));
-                pe.Graphics.FillRectangle(slicesBrush, control.SlicesContainerRectangle);
+                Brush textBrush = new SolidBrush(textColor);
+                Point offset = new Point(10, 10);
+
+                Pen textPen = new Pen(textBrush, 2);                
+                textPen.DashStyle = DashStyle.Dash;
+                textPen.Alignment = PenAlignment.Inset;
+
+                pe.Graphics.DrawString("RulerContainer", SystemFonts.DefaultFont, textBrush, 
+                    control.RulerContainerRectangle.X + offset.X, control.RulerContainerRectangle.Y + offset.Y);
+                pe.Graphics.DrawString("SlicesContainer", SystemFonts.DefaultFont, textBrush, 
+                    control.SlicesContainerRectangle.X + offset.X, control.SlicesContainerRectangle.Y + offset.Y);
+
+                pe.Graphics.DrawRectangle(textPen, control.RulerContainerRectangle);
+                pe.Graphics.DrawRectangle(textPen, control.SlicesContainerRectangle);
             }            
 
             public override IList SnapLines
@@ -42,6 +56,15 @@ namespace Scissors.Timeline
 
                     return snapLines;
                 }
+            }
+
+            public Color SelectHighestContrast(Color compared, Color color1, Color color2)
+            {
+                float L1 = compared.GetBrightness();
+                float L2 = color1.GetBrightness();
+                float L3 = color2.GetBrightness();
+
+                return (L1 + 0.05) / (L2 + 0.05) > (L3 + 0.05) / (L1 + 0.05) ? color1 : color2;
             }
         }
     }
