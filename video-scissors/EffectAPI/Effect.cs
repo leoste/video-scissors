63d73fb1b14c55a3da334982ba19f286e81f7f59
GitWither <wither.tweets@gmail.com>
2019-03-23 14:21:29 +0100

    Added an instantiatable effect class which will represent every effect

---
+++ video-scissors/EffectAPI/Effect.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scissors.EffectAPI
+{
+    class Effect
+    {
+        private readonly EffectInfo info;
+        private readonly IEffect effect;
+
+        public EffectInfo Info
+        {
+            get
+            {
+                return info;
+            }
+        }
+
+        public IEffect EffectInstance
+        {
+            get
+            {
+                return effect;
+            }
+        }
+
+        public Effect(EffectInfo info, IEffect effect)
+        {
+            this.info = info;
+            this.effect = effect;
+        }
+    }
+}
