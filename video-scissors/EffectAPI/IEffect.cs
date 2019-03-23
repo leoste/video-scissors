2d7c21f77fb90254ca26ec4f2bb3728ffb414522
GitWither <wither.tweets@gmail.com>
2019-03-23 17:18:20 +0100

    Removed useless using statements from IEffect

--- video-scissors/EffectAPI/IEffect.cs
+++ video-scissors/EffectAPI/IEffect.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Scissors.EffectAPI
+﻿namespace Scissors.EffectAPI
 {
     public interface IEffect
     {
