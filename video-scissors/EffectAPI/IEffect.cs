dde7490e947eeb7d3f1e50692b0ea40d74c4c76e
GitWither <wither.tweets@gmail.com>
2019-03-23 13:32:06 +0100

    Added custom effect interface

---
+++ video-scissors/EffectAPI/IEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scissors.EffectAPI
+{
+    public interface IEffect
+    {
+
+        void OnLoad();
+        void Update();
+        void OnUnload();
+    }
+}
