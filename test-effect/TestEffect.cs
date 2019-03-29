d4d652981c941034907ec2565c58ee7aff0bb2e1
GitWither <wither.tweets@gmail.com>
2019-03-29 23:37:12 +0100

    Updated test-effect to be more testable

--- test-effect/TestEffect.cs
+++ test-effect/TestEffect.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Scissors.EffectAPI;
 
 namespace test_effect
 {
-    [EffectInfo("test_effect", "Meme Deep Fryer", "Makes your videos have the deep fryer meme effect :joy: :ok_hand:", new int[] {0, 0, 1})]
+    [EffectInfo("test_effect", "Meme Deep Frier", "Makes your videos have the deep frier meme effect :joy: :ok_hand:", new int[] {5, 35, 2})]
     class TestEffect : IEffect
     {
         public void OnLoad()
         {
-            Console.WriteLine($"{Math.Sin(9)}");
+            Console.WriteLine("test_effect loaded!");
         }
 
         public void OnUpdate()
