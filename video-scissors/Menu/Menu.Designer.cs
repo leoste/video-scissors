32589ce75d70caca2d1ff50b59630650f1ae10c3
Karl Aleksander Leoste <KarlAleksander.Leoste@tptlive.ee>
2019-03-31 15:03:49 +0300

    A bit more work, looks cooler now

--- video-scissors/Menu/Menu.Designer.cs
+++ video-scissors/Menu/Menu.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.fancySidemenu1 = new Scissors.Controls.FancySidemenu();
             this.verticalScrollbar = new Scissors.Controls.FancyScrollbar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // fancySidemenu1
@@ -41,7 +42,7 @@
             this.fancySidemenu1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.fancySidemenu1.HighlightColor = System.Drawing.Color.Gray;
             this.fancySidemenu1.Location = new System.Drawing.Point(0, 0);
-            this.fancySidemenu1.Margin = new System.Windows.Forms.Padding(0);
+            this.fancySidemenu1.Margin = new System.Windows.Forms.Padding(0, 0, 3, 0);
             this.fancySidemenu1.Name = "fancySidemenu1";
             this.fancySidemenu1.SelectedId = 0;
             this.fancySidemenu1.Size = new System.Drawing.Size(100, 200);
@@ -54,10 +55,10 @@
             // verticalScrollbar
             // 
             this.verticalScrollbar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.verticalScrollbar.BackColor = System.Drawing.Color.Gray;
             this.verticalScrollbar.ForeColor = System.Drawing.Color.DarkGray;
-            this.verticalScrollbar.Location = new System.Drawing.Point(103, 0);
+            this.verticalScrollbar.Location = new System.Drawing.Point(376, 0);
             this.verticalScrollbar.Margin = new System.Windows.Forms.Padding(3, 0, 0, 0);
             this.verticalScrollbar.Maximum = 10;
             this.verticalScrollbar.Minimum = 0;
@@ -68,11 +69,20 @@
             this.verticalScrollbar.TabIndex = 13;
             this.verticalScrollbar.Value = 3;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(103, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(270, 200);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.fancySidemenu1);
             this.Controls.Add(this.verticalScrollbar);
             this.Name = "Menu";
@@ -85,5 +95,6 @@
 
         private Scissors.Controls.FancyScrollbar verticalScrollbar;
         private Controls.FancySidemenu fancySidemenu1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
