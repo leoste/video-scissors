340820f58a50c390a38c637775c2cdf62bbe32b8
GitWither <wither.tweets@gmail.com>
2019-04-02 21:34:00 +0200

    Refactored EffectManager to handle effect deletion and saving

--- video-scissors/EffectAPI/EffectManager.cs
+++ video-scissors/EffectAPI/EffectManager.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Reflection;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Windows.Forms;
 
 namespace Scissors.EffectAPI
 {
@@ -9,15 +13,67 @@ namespace Scissors.EffectAPI
     {
         public static List<Effect> Effects { get; } = new List<Effect>();
 
-        public static Effect LoadEffect(string path)
+        public static Effect LoadEffectFile(string path)
         {
+            string copyDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Video Scissors\\effects\\");
+            string fileName = Path.GetFileName(path);
+            if (!File.Exists(Path.Combine(copyDirectory, fileName)))
+            {
+                try
+                {
+                    File.Copy(path, Path.Combine(copyDirectory, fileName));
+                    File.SetAttributes(Path.Combine(copyDirectory, fileName), FileAttributes.Normal);
+                    return LoadEffect(path);
+                }
+                catch (ReflectionTypeLoadException e)
+                {
+                    MessageBox.Show(e.StackTrace, "Error!", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Duplicate effect detected!", "Duplicate Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return null;
+            }
+        }
+
+        public static void LoadEffects()
+        {
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Video Scissors\\effects\\");
+            if (Directory.Exists(path))
+            {
+                foreach (string effectFile in Directory.GetFiles(path))
+                {
+                    if (Path.GetExtension(effectFile) == ".dll")
+                    {
+                        LoadEffect(effectFile);
+                    }
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public static void UnloadEffect(Effect effect)
+        {
+            effect.EffectInstance.OnUnload();
+            Effects.Remove(effect);
+            File.Delete(effect.Path);
+        }
+
+        private static Effect LoadEffect(string path)
+        {
+            string copyDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Video Scissors\\effects\\");
             try
             {
                 Assembly effectDll = Assembly.LoadFile(path);
                 Type type = effectDll.GetTypes()[0];
                 IEffect effectInstance = (IEffect)Activator.CreateInstance(type);
                 EffectInfo effectInfo = (EffectInfo)type.GetCustomAttribute(typeof(EffectInfo), false);
-                Effect effect = new Effect(effectInfo, effectInstance);
+                Effect effect = new Effect(effectInfo, effectInstance, Path.Combine(copyDirectory, Path.GetFileName(path)));
 
                 effect.EffectInstance.OnLoad();
                 Effects.Add(effect);
@@ -28,12 +84,6 @@ namespace Scissors.EffectAPI
                 Debug.Fail(e.StackTrace);
                 return null;
             }
-        }
-
-        public static void UnloadEffect(Effect effect)
-        {
-            effect.EffectInstance.OnUnload();
-            Effects.Remove(effect);
         }
     }
 }
